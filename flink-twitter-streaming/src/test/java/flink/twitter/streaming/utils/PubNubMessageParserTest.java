a213c3eefd51194c9b940dfc5c0d1bbd3f532f3c
hophuong1983 <51144801+hophuong1983@users.noreply.github.com>
2023-05-18 08:38:22 +0700

    handle null place in tweet obj, remove log4j dependency
    Handle the null place entity in json tweet received
    Remove log4j dependency

--- flink-twitter-streaming/src/test/java/flink/twitter/streaming/utils/PubNubMessageParserTest.java
+++ flink-twitter-streaming/src/test/java/flink/twitter/streaming/utils/PubNubMessageParserTest.java
@@ -33,6 +33,7 @@ class PubNubMessageParserTest {
 
     @Test
     void convertToTweet2() throws IOException {
+        // Empty hash tags
         String payload = "{\n" +
                 "  \"created_at\": \"Thu Feb 09 13:38:50 +0000 2023\",\n" +
                 "  \"id\": 1623677806713487400,\n" +
@@ -67,4 +68,37 @@ class PubNubMessageParserTest {
         assertEquals(expected, PubNubMessageParser.convertToTweet(msg));
     }
 
+    @Test
+    void convertToTweet3() throws IOException {
+        // No country
+        String payload = "{\n" +
+                "  \"created_at\": \"Thu Feb 09 13:38:50 +0000 2023\",\n" +
+                "  \"id\": 1623677806713487400,\n" +
+                "  \"id_str\": \"1623677806713487361\",\n" +
+                "  \"text\": \"INDUSTRY MIXER February 18th\",\n" +
+                "  \"source\": \"<a href=\\\"http://instagram.com\\\" rel=\\\"nofollow\\\">Instagram</a>\",\n" +
+                "  \"user\": {\n" +
+                "    \"id\": 1002973047081656300,\n" +
+                "    \"id_str\": \"1002973047081656321\",\n" +
+                "    \"name\": \"ATL BABY G\"" +
+                "  },\n" +
+                "  \"place\": null,\n" +
+                "  \"entities\": {\n" +
+                "    \"hashtags\": []" +
+                "  },\n" +
+                "  \"timestamp_ms\": \"1675949930252\"\n" +
+                "}";
+
+        JsonElement msg = new JsonParser().parseString(payload);
+        Tweet expected = new Tweet(
+                "1623677806713487361",
+                1675949930252L,
+                "INDUSTRY MIXER February 18th",
+                "ATL BABY G",
+                null,
+                new String[0]
+        );
+
+        assertEquals(expected, PubNubMessageParser.convertToTweet(msg));
+    }
 }
